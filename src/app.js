9df186170732d9bea9fb1d6802e84b395b201367
sebastianlopezs <135356217+sebastianlopezs@users.noreply.github.com>
2023-08-16 04:38:04 +0000

    listo

--- src/app.js
+++ src/app.js
@@ -2,10 +2,25 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronouns = ["el", "vuestro", "nuestro", "mi"];
+  let adjs = ["gran", "mejor", "super", "increible"];
+  let nouns = ["amigo", "perro", "plei5", "carro"];
+  let extensions = [".com", ".net", ".org", ".ve"];
+  let results = "";
+
+  pronouns.forEach(pronoun => {
+    adjs.forEach(adj => {
+      nouns.forEach(noun => {
+        extensions.forEach(extension => {
+          console.log(pronoun + adj + noun + extension);
+          results = results + "\n" + pronoun + adj + noun + extension;
+        });
+      });
+    });
+  });
+  let domainGenerator = document.getElementById("results");
+  domainGenerator.innerHTML = results;
 };
